031ff25d86cf94d989c2dd457ffc1e5d5ad2c3a0
Oleg Berlo <oleg.b@oxtech.org>
2022-05-22 00:45:07 +0300

    Base Layout / 21.05.22

--- index.js
+++ index.js
@@ -1,66 +1,31 @@
-const http = require('http');
+const express = require('express');
 const path = require('path');
-const fs = require('fs');
-const server = http.createServer((req, res) => {
-    if(req.method === 'GET') {
-        res.writeHead(200, {
-            'Content-Type': 'text/html; charset=utf-8'
-        });
+const exphbs = require('express-handlebars');
 
-        if(req.url === '/') {
-            fs.readFile(path.join(__dirname, 'views', 'index.html'), 'utf-8', (err, content) => {
-                if(err) {
-                    throw err
-                };
+const app = express();
 
-                res.end(content)
-            })
-        }
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
+})
 
-        if(req.url === '/about') {
-            fs.readFile(path.join(__dirname, 'views', 'about.html'), 'utf-8', (err, content) => {
-                if(err) {
-                    throw err
-                };
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', 'views');
 
-                res.end(content)
-            })
-        }
-
-        if(req.url === '/api/users') {
-            res.writeHead(200, {
-                'Content-type': 'text/json'
-            })
-
-            const users = [{name: 'Oleg'}, {name: 'Julia'}]
-
-            res.end(JSON.stringify(users))
-        }
-    }
-
-    if(req.method === 'POST') {
-        const body = []
-
-        res.writeHead(200, {
-            'Content-Type': 'text/html; charset=utf-8'
-        })
-
-        req.on('data', data => {
-            body.push(Buffer.from(data));
-        });
-
-        req.on('end', () => {
-            const message = body.toString().split('=')[1];
-
-            res.end(`
-            <h1>Russian worship fucking self: ${message}</h1>
-        `)
-        })
-
-
-    }
+app.get('/', (request, responce) => {
+    responce.render('index');
 });
 
-server.listen(3001, () => {
-    console.log('server is running...')
-})
+app.get('/about', (request, responce) => {
+    responce.render('about');
+});
+
+
+
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
