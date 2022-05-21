7798a6694613243a104b1a1012316b6b6c3b80fa
Oleg Berlo <oleg.b@oxtech.org>
2022-05-21 23:31:31 +0300

    Work with console / 21.05.22

--- index.js
+++ index.js
@@ -1,19 +1,41 @@
 const http = require('http');
-
+const path = require('path');
+const fs = require('fs');
 const server = http.createServer((req, res) => {
     if(req.method === 'GET') {
         res.writeHead(200, {
             'Content-Type': 'text/html; charset=utf-8'
         });
-        res.end(
-            `
-                <h1>Form</h1>
-                <form method="post" action="/">
-                    <input name="title" type="text">
-                    <button type="submit">submit</button>
-                </form>
-            `
-        )
+
+        if(req.url === '/') {
+            fs.readFile(path.join(__dirname, 'views', 'index.html'), 'utf-8', (err, content) => {
+                if(err) {
+                    throw err
+                };
+
+                res.end(content)
+            })
+        }
+
+        if(req.url === '/about') {
+            fs.readFile(path.join(__dirname, 'views', 'about.html'), 'utf-8', (err, content) => {
+                if(err) {
+                    throw err
+                };
+
+                res.end(content)
+            })
+        }
+
+        if(req.url === '/api/users') {
+            res.writeHead(200, {
+                'Content-type': 'text/json'
+            })
+
+            const users = [{name: 'Oleg'}, {name: 'Julia'}]
+
+            res.end(JSON.stringify(users))
+        }
     }
 
     if(req.method === 'POST') {
@@ -39,6 +61,6 @@ const server = http.createServer((req, res) => {
     }
 });
 
-server.listen(3000, () => {
+server.listen(3001, () => {
     console.log('server is running...')
 })
